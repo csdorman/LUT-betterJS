4140bef822aa0532089b8014fb776452b376b417
csdorman <csdorman@gmail.com>
2021-12-01 11:45:41 -0500

    Finished Destructuring.

--- index.js
+++ index.js
@@ -25,5 +25,56 @@ console.log(newArray2)
 
 const sayHi = () => console.log("Hey there.")
 
-// Stopped at 6:30
 sayHi()
+
+// Object Destructuring
+
+// Creating objects
+// const person = {
+//     name: "Colin",
+//     age: 39,
+//     job1: "Musician",
+//     job2: "WebDev",
+//     hobby: "Guitar"
+// }
+
+//The OLD way to write a function to create an object:
+// const makePerson = (name, age, job1) => {
+//     return {
+//         name: name,
+//         age: age,
+//         job1: job1
+//     }
+// }
+
+// New way allows you to omit property and value name IF they are the same
+const makePerson = (name, age, job1) => {
+    return {
+        name,
+        age,
+        job1
+    }
+}
+//console.log(makePerson("colin",  39, "musician"))
+
+
+const dev = makePerson("john", 33, "developer")
+console.log(dev)
+// Old way to get specific data out of object
+// These are confusing since they involve creating new variables or re-naming items that already have names
+// const name = dev.name
+// const devName = dev.name
+
+//Easier way:
+const {name} = dev
+console.log(name)
+/*
+You can also pull out multiple items from the object:
+const {name, age} = dev
+console.log(name, age) // john, 33
+
+Or you can use the spread operator (...rest)
+const {name, ...rest} = dev
+console.log(name, rest) // john, {age: 33, job1: "developer"}
+ */
+
