337e72a87cfa37f5a3db2e4068acdda281a949a8
csdorman <csdorman@gmail.com>
2021-12-06 12:04:57 -0500

    Started Immutable & Pure functions

--- index.js
+++ index.js
@@ -5,14 +5,41 @@ document.getElementById("app").innerHTML = `
     </div>
 `;
 /*
-Naming Things
+Immutable and Pure Functions
+
+Immutable vs. Mutable
+- Can't be vs can be change
+- In general, you want to use things that can't (or won't) change the data
+
  */
+
+// This isn't *bad*, but it is brittle
+// This uses the *same variable name* "name" for *different data* (first vs. full name)
+// let name = "Steve" //first name
+// name = name + " Smithers" //adding a last name
+// console.log(name) //Steve Smithers
+
+//Better
+// When the data content changes, the variable name *also* changes
+const name = "Steve"
+const fullName = name + " Smithers"
+console.log(name)
+
+// RESUME: 5:20
+
+const BASE_SALARY = 16000
+const SALARY_MULTIPLIER = 4
+
+// "ytd" would need a comment or other annotation to make sure its meaning is clear
+const ytd = 100000
+const salesYearToDate = 100000
 
 const makePerson = ({firstName, lastName, age, job}) => {
     return {
         name: firstName + ' ' + lastName,
         age,
         job,
+        salary: BASE_SALARY * SALARY_MULTIPLIER,
     }
 }
 
@@ -22,3 +49,12 @@ const dev = makePerson({
     age: 39,
     job: "Musician",
 })
+
+const hireDev = ({ dev }) => {
+    const hiredDev = {
+        hired: true,
+        ...dev
+    }
+    return hiredDev
+}
+console.log(hireDev({dev}))
