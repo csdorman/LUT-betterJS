04688ab297f1d80808bd1a4c8a9da64529e6b6b8
csdorman <csdorman@gmail.com>
2021-12-07 11:55:16 -0500

    Starting on Benefits of Smaller Functions

--- index.js
+++ index.js
@@ -5,54 +5,44 @@ document.getElementById("app").innerHTML = `
     </div>
 `;
 /*
-Immutable and Pure Functions
-
-Immutable vs. Mutable
-- Can't be vs can be change
-- In general, you want to use things that can't (or won't) change the data - immutability is better than mutable
-
+Benefits of Smaller Functions
  */
 
-// This isn't *bad*, but it is brittle
-// This uses the *same variable name* "name" for *different data* (first vs. full name)
-// let name = "Steve" //first name
-// name = name + " Smithers" //adding a last name
-// console.log(name) //Steve Smithers
+const cart = [10, 5, 15] // value of items in cart
 
+// If you have a single parameter, you can use NO parenthesis in arrow function
+const fakeAPICharge = total => true //simulates API call (to Stripe or similar). Always returns true here.
+const fakeSendReceipt = total => true //simulates sending a receipt email. Always returns true here
+
+// This is NOT a great function - very untestable
+// Doesn't test if total is correct, if subtotal correct, if email goes through, etc.
+// Too much is going on within a single function
 /*
-BETTER
-When the content changes, the variable name also changes.
-In general, you should NOT update variables that often - it usually makes more sense to create a NEW variable
- */
-
-const name = "Steve"
-const fullName = name + " Smithers"
-console.log(name)
-
-/*
-PURE FUNCTIONS
-- A function that will ALWAYS return the same thing with the same inputs
-- Often times code will return different things within the same function based upon EXTERNAL variables - this is not ideal
-- This keeps state internal to the function
-- Functions should do ONE THING and do it the SAME EVERY TIME
-- PURE functions are much easier to test
- */
-
-const addTwo = (x) => x + 2 //this is pure - it will always return the same thing
-console.log(addTwo(2))
-console.log(addTwo(2))
-
-let multi = 3
-const addThree = (x) => x + multi // NOT Pure - uses an external variable that can change
-console.log(addThree(2))
-multi = 4
-console.log(addThree(2))
-
-let mult = 2
-const addFour = (x) => { // Also an impure function
-    mult += 2  // This function modifies an external state - which will change what it outputs outside of the function
-    return x + mult
+const checkout = cart => {
+    let total = reduce((tempTotal, item) => tempTotal + item)
+    total = total + 10
+    const orderSuccess = fakeAPICharge(total)
+    if (orderSuccess) {
+        fakeSendReceipt({
+            email: "norealemail@gmail.com",
+            total
+        })
+    }
+    return orderSuccess
 }
-console.log(addFour(2))
-console.log(addFour(2))
-console.log(addFour(2))
+*/
+
+// Better
+const SHIPPING_COST = 10
+const getSubtotal = cart => cart.reduce((tempTotal, item) => tempTotal + item) // subtotal functionality goes here since it's it's own thing. Now you can test subTotal to make sure it works - simply pass an array of numbers.
+const getTotal = subTotal => subTotal + SHIPPING_COST // Add subTotal value and shipping. Also easily testable.
+
+
+const checkout = cart => {
+    const subTotal = getSubtotal(cart) //move subTotal functionality to own function, clear variable name
+    const total = getTotal(subTotal) // move total functionality to its own function.
+}
+
+// RESUME at 8:00
+
+checkout(cart)
