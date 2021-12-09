f520ac606edebbd71c75420a50e8c6a057791457
csdorman <csdorman@gmail.com>
2021-12-09 11:28:28 -0500

    Finished Interacting with the DOM

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 /*
-JavaScript & the DOM (+ MDN Docs)
+Interacting with the DOM
  */
 
-document.getElementById("app").innerHTML = `
-    <h1>Hello Parcel!</h1>
-    <div>
-        Look here for more information about Parcel
-    </div>
-    <div>
-    </div>
-</div>
-`;
+const logo = document.getElementById("logo")
+logo.innerText = "Yo yo yo!"
 
-// A Property of the DOM - properties can be viewed (or set)
-console.log(document.body) //returns the body of the DOM
-console.log(document.URL) //returns the URL of the document
-
-// A Method can be run to make things
-const headingFour = document.createElement("h4")
-headingFour.innerHTML = "<span>This is coming from inside the DOM! at </span> " + document.URL
-console.log(headingFour) // <h4><span>This is coming from inside the DOM!</span></h4>
-document.body.appendChild(headingFour)
+const heading = document.createElement("h1")
+heading.innerHTML = "<span>Hello from inside the DOM!</span> " + document.URL
+document.body.appendChild(heading)
