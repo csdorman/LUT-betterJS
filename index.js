de6aabcc9d23f1c9bfe0b66db66fe7f964468054
csdorman <csdorman@gmail.com>
2021-12-16 11:54:19 -0500

    Finished Async Await

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Promises
+Async Await
  */
 
 // sample "import" from Stripe (charging a credit card)
@@ -25,16 +25,39 @@ chargeCard('12341234123', (result) => {
 // A "promise" in JS is the same as a promise in real life - if someone promises you $10, they haven't given you money, but they have said that they will give you the money at a future date.
 // Many API calls return promises
 
-const chargeCard = () => new Promise((resolve, reject)  => {
-    // card is valid
-    if (true) {
-        return resolve(true)
-    }
-    return resolve(false) // This isn't "rejecting", this is just resolving to false (IE credit card number was incorrect and the charge didn't go through
-    reject("FAIL FAIL")
-})
+//Using a PROMISE
+/*
+const chargeCC = (ccNumber) => {
+    const chargeCard = (ccNumber) => new Promise((resolve, reject)  => {
+        // card is valid
+        if (true) {
+            return resolve(true)
+        }
+        return resolve(false) // This isn't "rejecting", this is just resolving to false (IE credit card number was incorrect and the charge didn't go through
+        reject("FAIL FAIL")
+    })
+}
+*/
 
-console.log(chargeCard())
+// Using Async/Await
+// Uses promises "under the hood". As long as you're returning promises, you can use async/await
+
+const chargeCC = async (ccNumber) => {
+    try {
+        const result = await chargeCard(ccNumber)
+        console.log(result)
+        return result
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+const chargeCard = (ccNumber) => {
+    return true
+}
+
+
+console.log(chargeCC(12312234122))
 
 // Once the promise returns data
 chargeCard().then((val) => {
