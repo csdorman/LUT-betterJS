70853dc3923aa871b9bfdbb61e78a7e3eebace68
csdorman <csdorman@gmail.com>
2021-12-22 11:33:20 -0500

    Finished This (second watch)

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ console.log(this)
 const test = {
     name: "test",
     testFunc: function() {
-        console.log(this.name) // this inside an object method. Returns *both* object methods
+        console.log(this.name)
+        console.log(this) // this inside an object method. Returns *both* object methods
         this.name = "Colin"
         console.log(this.name)
     },
@@ -27,11 +28,12 @@ function team() {
 }
 
 const button = document.getElementById("new-colors")
-button.addEventListener("click", function() {
-    console.log(this)
+button.addEventListener("click", function() { // can't use an arrow function, or its undefined
+    console.log(this) // refers to the context, the actual button in this case
+    this.innerText = "Clicked!" // this will change the text on the button
 })
 
-test.testFunc()
-test.testFuncTwo()
-team() //outputs undefined (or window). Not creating any context
-const football = new team() // creates new constructor
+test.testFunc() // returns object and BOTH object methods (testFunc and testFuncTwo)
+test.testFuncTwo() // since this uses an arrow function, it's undefined
+team() //outputs undefined (or window). Not creating any context.
+const football = new team() // creates new constructor which will output "team"
